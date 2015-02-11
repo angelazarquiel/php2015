a9fc52ccd59c17eb7e9059ff7137302c416844fa
usuario <usuario@192.168.195.180>
2015-02-11 21:30:47 +0100

    primera versión

---
+++ ejemplos/clase1/cadenas.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hola mundo</title>
+</head>
+<body>
+
+<?php
+print "una cadena entre comillas<br>";
+print 'una cadena entre comillas simples<br>';
+print "una cadena: 'cadena'";
+print "una cadena: \"cadena\"";
+?>
+
+</body>
+</html>
---
+++ ejemplos/clase1/holamundo.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hola mundo</title>
+</head>
+<body>
+
+<h1>Mi primera página</h1>
+
+<?php
+# mi primer párrafo
+print "<p>hola mundo</p>";
+?>
+<h2>Segunda fase</h2>
+<?php 
+print "<p>Adiós</p>\n";
+?>
+</body>
+</html>
